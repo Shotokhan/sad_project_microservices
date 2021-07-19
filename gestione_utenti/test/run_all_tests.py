2eff028c31b64ab89cb16cc72096077b126b3174
marcofelix98 <marcocarlofeliciano@yahoo.it>
2021-07-19 15:08:57 +0200

    Forgot to add operatore_db_test to run_all_tests in gestione_utenti.

--- gestione_utenti/test/run_all_tests.py
+++ gestione_utenti/test/run_all_tests.py
@@ -1,6 +1,7 @@
 import hash_pw_test
 import utente_db_test
 import paziente_db_test
+import operatore_db_test
 
 
 if __name__ == "__main__":
@@ -9,4 +10,6 @@ if __name__ == "__main__":
     for test in utente_db_test.tests:
         test()
     for test in paziente_db_test.tests:
+        test()
+    for test in operatore_db_test.tests:
         test()
